e03747d5e24b75c6e3b1470a0a9bb1c680d2cff6
Fernando Valente <fernando@fvalente.org>
2014-08-27 21:48:48 +0300

    Create main.m

---
+++ FVGetApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  FVGetApp
+//
+//  Created by Fernando Valente on 9/29/13.
+//  Copyright (c) 2013 Fernando Valente. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
